5b162a8d17a61f7d8f107804849f48e3f525ed77
Bolarg9764 <bolarg9764@gmail.com>
2022-11-14 07:58:59 -0500

    updates made after a long time

---
+++ js/app.js
@@ -0,0 +1,21 @@
+baguetteBox.run(".baguetteBoxThree", {
+  animation: "fadeIn",
+  noScrollbars: true,
+});
+
+/* baguetteBox.run(".gallery"); */
+
+const myInput = document.getElementById("myInput");
+
+/* console.log(myInput);
+myInput.addEventListener("keyup", (e) => {
+  let currentValue = e.target.value.toLowerCase();
+  let authors = document.querySelectorAll("img.image");
+  authors.forEach((author) => {
+    if (author.textContent.toLocaleLowerCase().includes(currentValue)) {
+      author.parentNode.style.display = "block";
+    } else {
+      author.parentNode.style.display = "none";
+    }
+  });
+}); */
---
+++ js/loops.js
@@ -0,0 +1,29 @@
+// const main = document.querySelector('gallery');
+// let html = '';
+
+// }
+
+//main.innerHTML = html;
+
+const newItem = () => {
+    for (let i = 1; i <= 12; i++) {
+        html =+ `<a href=\"photos/${i}.jpg\"><img src=\"photos/thumbnails/${i}.jpg\" class=\"image-${i}\"></a>`;
+        }
+    }
+
+
+
+
+
+// }
+$('.gallery').append(function() {
+
+
+});
+
+var image = document.createElement('IMG');
+var a = document.createElement('A');
+
+var newItem = a.appendChild(image);
+
+let newItem =+
